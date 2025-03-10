b15be8d1574c33c6ac145823c6d1fdaf9e88d0fa
Sarai Gabriela <103385050+saraiherrerab@users.noreply.github.com>
2025-03-09 22:57:33 -0400

    incorporando eventos de movimiento

--- Frontend/Sistema-Educativo-Pcomputacional/src/App.tsx
+++ Frontend/Sistema-Educativo-Pcomputacional/src/App.tsx
@@ -17,9 +17,28 @@ function App() {
     }
   );
   juegoKaplay.loadRoot("./"); // A good idea for Itch.io publishing later
-  juegoKaplay.loadSprite("robot", "sprites/robot-preview.png");
-  const player= juegoKaplay.add([juegoKaplay.pos(0,0),juegoKaplay.sprite("robot")]);
+  juegoKaplay.loadSprite("robot", "sprites/robotin.png", {
+    sliceX: 4, // how many sprites are in the X axis
+    sliceY: 12, // how many sprites are in the Y axis
+    anims: {
+        right: { from: 16, to: 19, loop: false },
+        up:{from: 20, to: 23, loop: false},
+        down:{from: 12, to: 15, loop: false},
+        quiet:{from: 0, to: 0, loop: false},
+    },
+});
+  juegoKaplay.loadSprite("up", "sprites/up-arrow.png");
+  juegoKaplay.loadSprite("down", "sprites/down-arrow.png");
+  juegoKaplay.loadSprite("left", "sprites/left-arrow.png");
+  juegoKaplay.loadSprite("right", "sprites/right-arrow.png");
+  const player= juegoKaplay.add([juegoKaplay.pos(0,0),juegoKaplay.sprite("robot"), juegoKaplay.scale(5), juegoKaplay.area()]);
+  const upArrow= juegoKaplay.add([juegoKaplay.pos(500,500),juegoKaplay.sprite("up"), juegoKaplay.scale(0.8), juegoKaplay.area()]);
+  const downArrow= juegoKaplay.add([juegoKaplay.pos(700,700),juegoKaplay.sprite("down"),juegoKaplay.scale(0.8), juegoKaplay.area()]);
+  const leftArrow= juegoKaplay.add([juegoKaplay.pos(0,100),juegoKaplay.sprite("left"), juegoKaplay.scale(0.8), juegoKaplay.area()]);
+  const rightArrow= juegoKaplay.add([juegoKaplay.pos(700,100),juegoKaplay.sprite("right"), juegoKaplay.scale(0.8), juegoKaplay.area()]);
   const velocidad = 200
+
+  
   juegoKaplay.onKeyDown("w", () => {
       player.move(0, -velocidad);
   });
@@ -33,6 +52,28 @@ function App() {
   juegoKaplay.onKeyDown("d", () => {
       player.move(velocidad, 0);
   });
+
+
+  
+  upArrow.onClick(() => {
+    player.move(0, -velocidad);
+    player.play("up");
+  });
+
+  downArrow.onClick( () => {
+    player.move(0, velocidad);
+    player.play("down");
+  });
+
+  leftArrow.onClick( () => {
+    player.move(-velocidad, 0);
+  });
+
+  rightArrow.onClick( () => {
+    player.move(velocidad, 0);
+    player.play("right");
+  });
+
 
   return (
     <>
