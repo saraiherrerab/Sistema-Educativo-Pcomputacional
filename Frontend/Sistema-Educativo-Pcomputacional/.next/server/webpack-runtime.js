d324d0c9ae8d108e4e195b39d6f29564eebf9174
Lu27656348 <luiscarlossomoza@gmail.com>
2025-05-14 21:04:22 -0600

    Colisión de edificios con Panel

--- Frontend/Sistema-Educativo-Pcomputacional/.next/server/webpack-runtime.js
+++ Frontend/Sistema-Educativo-Pcomputacional/.next/server/webpack-runtime.js
@@ -125,11 +125,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	(() => {
-<<<<<<< HEAD
-/******/ 		__webpack_require__.h = () => ("29262a6918f493b0")
-=======
-/******/ 		__webpack_require__.h = () => ("9e2d83c02b408f23")
->>>>>>> origin/LaSaraRama
+/******/ 		__webpack_require__.h = () => ("e072504f4d250513")
 /******/ 	})();
 /******/ 	
 /******/ 	/* webpack/runtime/hasOwnProperty shorthand */
--- Frontend/Sistema-Educativo-Pcomputacional/src/app/Curso/page.tsx
+++ Frontend/Sistema-Educativo-Pcomputacional/src/app/Curso/page.tsx
@@ -56,6 +56,13 @@ export default function AdministradoresLista() {
     
 
     const [cursoEditando, setCursoEditando] = useState<Curso | null>(null);
+    const [grupoEditando, setGrupoEditando] = useState<Grupo>(
+        {
+            id_grupo: 0,
+            nombre_grupo: "string",
+            id_curso: 0
+        }
+    )
 
     const [nuevoCurso, setNuevoCurso] = useState<Curso>({
         id_curso: 0,
@@ -202,6 +209,7 @@ export default function AdministradoresLista() {
     })
 
     const [grupos, setGrupos] = useState<Grupo[]>([])
+    const [gruposFaltantes, setGruposFaltantes] = useState<Grupo[]>([])
 
     const [grupoActivo, setGrupoActivo] = useState<string | null>(null);
 
@@ -210,6 +218,10 @@ export default function AdministradoresLista() {
     const [mostrarAsignacion, setMostrarAsignacion] = useState<boolean>(false)
 
     const [mostrarFormularioGrupo, setMostrarFormularioGrupo] = useState<boolean>(false)
+
+    const [mostrarFormularioEditarGrupo, setMostrarFormularioEditarGrupo] = useState<boolean>(false)
+
+    mostrarFormularioEditarGrupo
 
     const toggleGrupo = async (id: number, nombre: string) => {
 
@@ -243,7 +255,7 @@ export default function AdministradoresLista() {
             const resultadoConsulta = await response.json()
             console.log(resultadoConsulta)
 
-            setGrupos(resultadoConsulta)
+            setGruposFaltantes(resultadoConsulta)
 
             setMostrarAsignacion(!mostrarAsignacion)
 
@@ -306,7 +318,8 @@ export default function AdministradoresLista() {
             },
             body: JSON.stringify(
                 {
-                    nombre_grupo: nuevoCurso.nombre_curso
+                    nombre_grupo: nuevoCurso.nombre_curso,
+                    id_curso: cursoEscogido.id_curso
                 }
             )
         });
@@ -315,6 +328,7 @@ export default function AdministradoresLista() {
         console.log(resultadoConsulta)
         console.log(nuevoCurso)
 
+        setGrupos([...grupos,resultadoConsulta])
         setMostrarFormularioGrupo(!mostrarFormularioGrupo)
 
     };
@@ -341,7 +355,7 @@ export default function AdministradoresLista() {
         console.log(resultadoConsulta)
 
         setMostrarAsignacion(!mostrarAsignacion)
-        setMostrarAsignacion(!mostrarAsignacion)
+
         console.log(
                 {
                     id_grupo: grupoSeleccionado.id_grupo,
@@ -352,12 +366,64 @@ export default function AdministradoresLista() {
 
     }
 
+    const eliminarGrupo = async (id_grupo_seleccionado: number) => {
+        console.log("eliminarGrupo")
+
+        const response = await fetch(`http://localhost:5555/grupos/${id_grupo_seleccionado}`, {
+            method: 'DELETE',
+            mode: 'cors',   // Habilita CORS
+            headers: {
+            'Content-Type': 'application/json'
+            }
+        }); 
+
+        const resultadoConsulta = await response.json()
+        console.log(resultadoConsulta)
+
+        setGrupos([...grupos.filter( (grupo) => grupo.id_grupo !== id_grupo_seleccionado)])
+    }
+
+    const editarGrupo = async (grupo_seleccionado: Grupo) => {
+        console.log("editarGrupo")
+
+        console.log(grupo_seleccionado)
+
+        console.log(mostrarTablaPrincipal)
+        console.log(mostrarGrupos)
+
+        setGrupoEditando({...grupo_seleccionado})
+
+        setMostrarTablaPrincipal(false)
+
+        setMostrarFormularioEditarGrupo(true)
+
+        console.log(grupoEditando)
+
+        /*
+        const response = await fetch(`http://localhost:5555/grupos/${id_grupo_seleccionado}`, {
+            method: 'DELETE',
+            mode: 'cors',   // Habilita CORS
+            headers: {
+            'Content-Type': 'application/json'
+            }
+        }); 
+
+        const resultadoConsulta = await response.json()
+        console.log(resultadoConsulta)
+
+        setGrupos([...grupos.filter( (grupo) => grupo.id_grupo !== id_grupo_seleccionado)])
+
+        */
+    }
+
     const agregarNuevoCurso = async () => {
         console.log("agregarNuevoCurso()")
         setMostrarFormularioGrupo(!mostrarFormularioGrupo)
     }
 
     const [cambioValorCurso, SetCambioValorCurso] = useState(false)
+
+    const [mostrarTablaPrincipal, setMostrarTablaPrincipal] = useState<boolean>(true)
 
     const handleChangeCurso = (e: React.ChangeEvent<HTMLSelectElement>) => {
 
@@ -373,6 +439,53 @@ export default function AdministradoresLista() {
         }
     };
 
+    const handleSubmitEditandoGrupo = async (e: React.FormEvent<HTMLFormElement>) => {
+
+        e.preventDefault(); // evita que se recargue la página
+        
+        console.log("handleSubmitEditandoGrupo")
+        const response = await fetch(`http://localhost:5555/grupos`, {
+                method: 'PUT',
+                mode: 'cors',   // Habilita CORS
+                headers: {
+                  'Content-Type': 'application/json'
+                },
+                body: JSON.stringify(
+                    {
+                        id_grupo: grupoEditando.id_grupo,
+                        nombre_grupo: grupoEditando.nombre_grupo
+                    }   
+                )
+            });
+
+        const resultadoConsulta = await response.json()
+        console.log(resultadoConsulta)
+
+        setGrupos((prevGrupos) =>
+            prevGrupos.map((grupo) =>
+            grupo.id_grupo === grupoEditando.id_grupo ? { ...grupo, ...grupoEditando } : grupo
+            )
+         );
+
+         setGrupoEditando(
+            {
+                id_grupo: 0,
+                id_curso: 0,
+                nombre_grupo: ""
+            }
+        )
+
+        setMostrarFormularioEditarGrupo(false)
+
+
+
+    }
+
+    const handleChangeEditandoGrupo = (e: React.ChangeEvent<HTMLInputElement>) => {
+        console.log(e.target.value)
+        setGrupoEditando({...grupoEditando, nombre_grupo: e.target.value})
+    }
+
     return (
         <>
             <Header
@@ -385,8 +498,10 @@ export default function AdministradoresLista() {
 
             <div className="listado">
 
+            { /* Mostrar Tabla Principal - Lista de Cursos */}
+
             {
-                !mostrarGrupos && 
+                mostrarTablaPrincipal && !mostrarGrupos && !mostrarFormulario && !cursoEditando && grupoEditando.id_grupo == 0 &&
                 (
                 <div className="encabezado">
                     <div className="tituloListado" style={{ cursor: 'pointer' }}>
@@ -415,59 +530,45 @@ export default function AdministradoresLista() {
                 )
             }
 
-            {!mostrarGrupos && mostrarFormulario && (
-                <div className="formulario-agregar">
-                    <h3>Agregar Nuevo Curso</h3>
-                    <input type="text" placeholder="Nombre" value={nuevoCurso.nombre_curso} onChange={e => setNuevoCurso({ ...nuevoCurso, nombre_curso: e.target.value })} />
-                    <button onClick={() => onAgregarCurso()}>Guardar</button>
-                    <button onClick={() => setMostrarFormulario(false)}>Cancelar</button>
-                </div>
-            )}
-
-            {!mostrarGrupos && !mostrarFormulario && !cursoEditando && (
-                <table>
-                    <thead>
-                        <tr>
-                            <th>ID</th>
-                            <th>Nombre</th>
-                            <th>Acciones</th>
-                            <th>Grupos</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        {cursosFiltrados.map((cursoSeleccionado) => (
-                            <tr key={cursoSeleccionado.id_curso}>
-                                <td>{cursoSeleccionado.id_curso ? cursoSeleccionado.id_curso : "null"}</td>
-                                <td>{cursoSeleccionado.nombre_curso ? cursoSeleccionado.nombre_curso : "null"}</td>
-                                
-                                <td>
-                                    <button onClick={() => onEditar(cursoSeleccionado)}>Editar</button>
-                                    <button onClick={() => onEliminar(cursoSeleccionado.id_curso)}>Eliminar</button>
-                                </td>
-                                <td>
-                                    <button onClick={() => gestionarGrupos(cursoSeleccionado)}>Grupos</button>
-                                </td>
-                            </tr>
-                        ))}
-                    </tbody>
-                </table>
-            )}
-
-            {!mostrarGrupos && cursoEditando && (
-                <div className="formulario-edicion">
-                    <h3>Editando curso</h3>
-                    <input type="text" placeholder="Nombre" value={cursoEditando.nombre_curso} onChange={e => setCursoEditando({ ...cursoEditando, nombre_curso: e.target.value })} />
-                    <button onClick={() => onGuardarEdicion()}>Guardar</button>
-                    <button onClick={() => setCursoEditando(null)}>Cancelar</button>
-                </div>
-            )}
-
             {
-                !mostrarFormularioGrupo && !mostrarAsignacion && mostrarGrupos && 
+                !mostrarGrupos && !mostrarFormulario && !cursoEditando && 
+                (
+                    <table>
+                        <thead>
+                            <tr>
+                                <th>ID</th>
+                                <th>Nombre</th>
+                                <th>Acciones</th>
+                                <th>Grupos</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            {cursosFiltrados.map((cursoSeleccionado) => (
+                                <tr key={cursoSeleccionado.id_curso}>
+                                    <td>{cursoSeleccionado.id_curso ? cursoSeleccionado.id_curso : "null"}</td>
+                                    <td>{cursoSeleccionado.nombre_curso ? cursoSeleccionado.nombre_curso : "null"}</td>
+                                    
+                                    <td>
+                                        <button onClick={() => onEditar(cursoSeleccionado)}>Editar</button>
+                                        <button onClick={() => onEliminar(cursoSeleccionado.id_curso)}>Eliminar</button>
+                                    </td>
+                                    <td>
+                                        <button onClick={() => gestionarGrupos(cursoSeleccionado)}>Grupos</button>
+                                    </td>
+                                </tr>
+                            ))}
+                        </tbody>
+                    </table>
+                )
+            }
+
+            { /* Mostrar Tabla - Lista de Cursos */}
+            
+            {
+                mostrarGrupos && !mostrarFormularioGrupo && !mostrarAsignacion && !mostrarFormulario && !mostrarFormularioEditarGrupo &&
                 (
                     <div>
-                        Hola
-                        <button onClick={() => setMostrarGrupos(!mostrarGrupos)}>Volver</button>
+                        <button onClick={() => setMostrarGrupos(!mostrarGrupos)}>Volver a Cursos</button>
                         <h2>Lista de Grupos</h2>
                         <button onClick={() => buscarGruposConCursoFaltante()}>Asignar Grupo a Curso</button>
                         <button onClick={() => agregarNuevoCurso()}>Agregar Nuevo Grupo</button>
@@ -487,8 +588,8 @@ export default function AdministradoresLista() {
                                     </td>
                                     <td >
                                         <div>
-                                            <button onClick={() => null}>Editar</button>
-                                            <button onClick={() => null}>Borrar</button>
+                                            <button onClick={() => editarGrupo(grupo)}>Editar</button>
+                                            <button onClick={() => eliminarGrupo(grupo.id_grupo)}>Borrar</button>
                                         </div>
                                         
                                     </td>
@@ -517,15 +618,15 @@ export default function AdministradoresLista() {
             }
 
             {
-                !mostrarFormularioGrupo && mostrarAsignacion && mostrarGrupos && 
+                mostrarGrupos && !mostrarFormularioGrupo && mostrarAsignacion  && !mostrarFormulario &&
                 (
                     <div>
                         Adios
-                        <button onClick={() => setMostrarGrupos(!mostrarGrupos)}>Volver</button>
+                        <button onClick={() => setMostrarAsignacion(!mostrarAsignacion)}>Volver a Grupos</button>
                         <h2>Lista de Grupos Faltantes</h2>
                         <select value={grupoSeleccionado.id_grupo} onChange={handleChange}>
                             <option value={0} disabled>-- Selecciona --</option>
-                            {grupos.map((grupo) => (
+                            {gruposFaltantes.map((grupo) => (
                             <option key={grupo.id_grupo} value={grupo.id_grupo}>
                                 {grupo.nombre_grupo}
                             </option>
@@ -536,6 +637,29 @@ export default function AdministradoresLista() {
                     
                 )
             }
+
+            {!mostrarGrupos && !mostrarFormularioGrupo && !mostrarAsignacion && mostrarFormulario && 
+                (
+                    <div className="formulario-agregar">
+                        <h3>Agregar Nuevo Curso</h3>
+                        <input type="text" placeholder="Nombre" value={nuevoCurso.nombre_curso} onChange={e => setNuevoCurso({ ...nuevoCurso, nombre_curso: e.target.value })} />
+                        <button onClick={() => onAgregarCurso()}>Guardar</button>
+                        <button onClick={() => setMostrarFormulario(false)}>Cancelar</button>
+                    </div>
+                )
+            }
+
+            
+
+            {!mostrarGrupos && cursoEditando && (
+                <div className="formulario-edicion">
+                    <h3>Editando curso</h3>
+                    <input type="text" placeholder="Nombre" value={cursoEditando.nombre_curso} onChange={e => setCursoEditando({ ...cursoEditando, nombre_curso: e.target.value })} />
+                    <button onClick={() => onGuardarEdicion()}>Guardar</button>
+                    <button onClick={() => setCursoEditando(null)}>Cancelar</button>
+                </div>
+            )}
+
 
             {
                 mostrarFormularioGrupo &&
@@ -556,6 +680,29 @@ export default function AdministradoresLista() {
                             <button type="submit">Enviar</button>
                         </form>
                         <button onClick={() => null} disabled = {true}>CREAR NUEVO GRUPO</button>
+                    </div>
+                    
+                )
+            }
+
+            {
+                !mostrarTablaPrincipal && mostrarFormularioEditarGrupo &&
+                (
+                    <div>
+                        <button onClick={() => {setMostrarFormularioEditarGrupo(false)}}>Volver</button>
+                        <h2>Formulario de Edición de Grupo</h2>
+                         <form onSubmit={handleSubmitEditandoGrupo}>
+                            <label htmlFor="nombre">Nombre:</label>
+                            <input
+                                type="text"
+                                id="nombre"
+                                value={grupoEditando.nombre_grupo}
+                                onChange={handleChangeEditandoGrupo}
+                                placeholder="Ingresa tu nombre"
+                            />
+                            <button type="submit">Enviar</button>
+                        </form>
+                        <button onClick={() => null} disabled = {true}>Confirmar Cambios</button>
                     </div>
                     
                 )
